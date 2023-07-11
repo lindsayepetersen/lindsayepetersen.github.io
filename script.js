30d0c4b0c8b573a3360dd2e331f883613453062c
[lindsayepetersen] <lindsayepetersen@gmail.com>
2023-07-10 18:52:44 -0600

    added chatbox and additional styling

---
+++ script.js
@@ -0,0 +1,7 @@
+function openChatWindow() {
+  document.getElementById("chat-form-container").style.display = "block";
+}
+
+function closeChatWindow() {
+  document.getElementById("chat-form-container").style.display = "block";
+}
