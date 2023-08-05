551d195a00ba0052f765b20a1181ad9343943ce2
[lindsayepetersen] <lindsayepetersen@gmail.com>
2023-08-05 00:41:14 -0600

    fixed positioning of elements

--- script.js
+++
@@ -1,9 +0,0 @@
-function openChatWindow() {
-  document.getElementById("chat-form-container").style.display = "block";
-}
-
-function closeChatWindow() {
-  document.getElementById("chat-form-container").style.display = "block";
-}
-
-export default script;
